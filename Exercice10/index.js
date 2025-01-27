5d2156f3bb2ed80bf51786cf5fab23007ceeb669
Govaerts Yoan <yoangovaerts@gmail.com>
2025-01-27 13:51:51 +0100

    ✅ exo 5,6,7,8,9 et 10

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,11 @@
 //votre code ici
+let result = '';
+
+function printNumbers(n){
+    for (let i = 1; i <= n; i++) {
+        result += i + ' ';
+    }
+    return result.trim();
+}
 
 export default printNumbers
